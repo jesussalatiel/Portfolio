3e04866c2399ebef95d390d064c427860319bd1a
Jesus Salatiel <jesus.bustamante@globant.com>
2025-01-12 15:38:27 -0600

    ci(workflow): added changes vue deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
+// https://vitejs.dev/config/
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import { resolve } from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    server: {
+        port: 3000,
+    },
+    plugins: [vue(), vueJsx()],
     resolve: {
         alias: {
-            "@": path.resolve(__dirname, "./src"),
+            "@": resolve(__dirname, "./src"),
+        },
+    },
+    build: {
+        outDir: "dist",
+        rollupOptions: {
+            input: resolve(__dirname, "index.html"),
         },
     },
 });
